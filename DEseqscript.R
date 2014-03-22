337702170dce32aa4000d57a114cf97937c46f70
fmacfarlane <f.macfarlane@dundee.ac.uk>
2014-03-22 16:47:16 +0000

    working R script

--- DEseqscript.R
+++ DEseqscript.R
@@ -1,10 +1,17 @@
+install.packages("DESeq2")
+install.packages("RCurl")
+source("http://bioconductor.org/biocLite.R")
+biocLite()
+biocLite("biomaRt")
+install.packages("stringr")
 require(DESeq2)
 require(RCurl)
 require(biomaRt)
 require(stringr)
+require(limma)
 
 
-geneCounts<-read.delim("/homes/fmacfarlane/Project/RNAseqCounts.txt",head=T,sep="\t",skip=1, row.names=1)
+geneCounts<-read.delim("~/GitHub/project/RNAseqCounts.txt",head=T,sep="\t",skip=1, row.names=1)
 
 nonZeroCounts<-geneCounts[rowSums(geneCounts[,6:28])>0,6:28]
 
@@ -19,7 +26,9 @@ dds$treatments <- relevel(dds$treatments, "l" )
 dds <- DESeq(dds)
 
 DEresults <- results(dds)
-
+#get results for padj
+res <- DEresults[order(DEresults$padj),]
+head(res)
 # get annotations
 require(biomaRt)
 mart <- useMart(biomart="ENSEMBL_MART_ENSEMBL", 
@@ -31,4 +40,23 @@ gg4 <- useDataset("ggallus_gene_ensembl",mart=mart)
 annot <- getBM(attributes=c("ensembl_gene_id","external_gene_id","affy_chicken"),filter="ensembl_gene_id",values=rownames(DEresults),mart=gg4)
 
 annotResults <- merge(annot,DEresults,by.x="ensembl_gene_id",by.y="row.names")
+colData(dds)
 
+
+#heat maps + plots
+#library("RColorBrewer")
+#library("gplots")
+#install.packages("gplots")
+#rld <- rlogTransformation(dds)
+#vsd <- varianceStabilizingTransformation(dds)
+#select <-order(rowMeans(counts(dds,normalized=TRUE)), decreasing=TRUE)[1:30]
+#hmcol <- colorRampPalette(brewer.pal(9, "GnBu"))(100)
+#heatmap.2(counts(dds,normalized=TRUE)[select,], col = hmcol,
+#          Rowv = FALSE, Colv = FALSE, scale="none",
+#          dendrogram="none", trace="none", margin=c(10,6))
+#heatmap.2(assay(rld)[select,], col = hmcol,
+#          Rowv = FALSE, Colv = FALSE, scale="none",
+#          dendrogram="none", trace="none", margin=c(10, 6))
+#heatmap.2(assay(vsd)[select,], col = hmcol,
+#          Rowv = FALSE, Colv = FALSE, scale="none",
+#          dendrogram="none", trace="none", margin=c(10, 6))
